b9e237577872fcf3ea46ed8634ed42f0a42dd576
Ning <ningke947@gmail.com>
2025-03-09 18:32:59 -0700

    first-draft

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//  NAVIGATION TOGGLE FUNCTION
+function toggleMenu() {
+    const menu = document.querySelector(".nav-menu");
+    menu.classList.toggle("responsive");
+  }
+
+  // TYPING EFFECT
+var typingEffect = new Typed(".typedText", {
+    strings: ["Ning Ke"],
+    loop: false,
+    typeSpeed: 100
+  });
+  
